12a21ccf2509a7da9b0c79b2342779290fb294c7
JBatsyus <ju.kalabina@gmail.com>
2024-09-25 22:53:24 +0700

    burger-scroll-to-link

--- build/js/index.js
+++ build/js/index.js
@@ -1,11 +1,11 @@
 const header = document.querySelector('.header');
 const headerHeight = header.offsetHeight;
-document.documentElement.style.setProperty('--js-header-height', headerHeight + "px");
+document.documentElement.style.setProperty('--js-header-height', headerHeight + 'px');
 //console.log("Высота хедера " + headerHeight + " пикселей");
 
-$(document).ready(function () {
-  let partnersSlider = new Swiper(".partners-slider", {
-    slidesPerView: "auto",
+$(document).ready(function() {
+  let partnersSlider = new Swiper('.partners-slider', {
+    slidesPerView: 'auto',
     slidesPerGroup: 1,
     loop: true,
     spaceBetween: 60,
@@ -19,87 +19,90 @@ $(document).ready(function () {
       768: {
         spaceBetween: 100,
       },
-    }
+    },
   });
 
- // модалка 
+  // модалка
 
+  $('.modal-toggle').on('click', function(e) {
+    e.preventDefault();
+    $('body').toggleClass('modal-open');
+    $('.modal').toggleClass('is-visible');
+  });
 
- $('.modal-toggle').on('click', function (e) {
-  e.preventDefault();
-  $('body').toggleClass('modal-open');
-  $('.modal').toggleClass('is-visible');
-});
-
-
-// якоря
-  $("a[href*='#']").on("click", function(e) {
+  // якоря
+  $("a[href*='#']").on('click', function(e) {
     e.preventDefault();
     var anchor = $(this);
     var target = $(anchor.attr('href'));
-    
+
     if (target.length) {
-        var offsetTop = target.offset().top - 100;
-        
-        // Анимация скролла
-        $('html, body').stop().animate({
-            scrollTop: offsetTop
-        }, 777);
+      var offsetTop = target.offset().top - 100;
+
+      // Анимация скролла
+      $('html, body')
+        .stop()
+        .animate(
+          {
+            scrollTop: offsetTop,
+          },
+          777
+        );
     }
     return false;
-});
+  });
 
-$('a[href^="#"]').on('click', function(e) {
+  $('a[href^="#"]').on('click', function(e) {
     e.preventDefault();
     var link = $(this).attr('href');
     var el = $(document).find(link);
-    
+
     if (el.length) {
-        var offsetTop = el.eq(0).offset().top - 100;
-        
-        // Анимация скролла
-        $('html, body').animate({
-            scrollTop: offsetTop
-        }, 1000, 'linear');
+      var offsetTop = el.eq(0).offset().top - 100;
+
+      // Анимация скролла
+      $('html, body').animate(
+        {
+          scrollTop: offsetTop,
+        },
+        1000,
+        'linear'
+      );
     }
     return false;
+  });
+
+  // мобильное меню
+  $('.menu-humb').on('click', function() {
+    $(this).toggleClass('active');
+    $('.menu-mob').toggleClass('active');
+    $('.header-bottom .container').toggleClass('menu_active');
+    $('.header-wrapper').toggleClass('bgk-hr');
+    $('body').toggleClass('no-scroll');
+  });
+
+  // прокрутка к анкорам
+  $('a.scroll-to').on('click', function(e) {
+    e.preventDefault();
+    let anchor = $(this).attr('href');
+    if ($('.menu-humb').hasClass('active')) {
+      $('body').removeClass('no-scroll');
+      $('.header-bottom .container').removeClass('menu_active');
+      $('.menu-mob').removeClass('active');
+      $('.menu-mob').removeClass('active');
+      $('.header-wrapper').removeClass('bgk-hr');
+      setTimeout(() => {
+        $('.menu-humb').removeClass('active');
+      }, 700);
+    }
+
+    $('html, body')
+      .stop()
+      .animate(
+        {
+          scrollTop: $(anchor).offset().top - 150,
+        },
+        700
+      );
+  });
 });
-
- // мобильное меню
- $('.menu-humb').on('click', function () {
-  $(this).toggleClass('active');
-  $('.menu-mob').toggleClass('active');
-  $('.header-bottom .container').toggleClass('menu_active');
-  $('.header-wrapper').toggleClass('bgk-hr');
-  $('body').toggleClass('no-scroll');
-});
-
-
-
-// $('a.scroll-to').on('click', function(e){
-//   e.preventDefault();
-//   var anchor = $(this).attr('href');
-  
-//   if ($('.hamburger').hasClass('is-open')) {
-//       $('body').removeClass('no-scroll');
-//       $('.hamburger, .mobile-menu').removeClass('is-open');
-//       $('.mobile-menu').addClass('is-closed');
-//       setTimeout(() => {
-//           $('.mobile-menu').removeClass('is-closed');
-//       }, 700);
-//   }
-
-//   $('html, body').stop().animate({
-//       scrollTop: $(anchor).offset().top - 50
-//   }, 800);
-// });
-
-
-});
-
-
-
-
-
-
-
